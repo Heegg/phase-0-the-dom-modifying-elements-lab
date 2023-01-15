9da61350a530e60681c214bd13b32c0a960a6789
Heeji Hyun <wlgmlgus@gmail.com>
2023-01-14 23:19:53 -0500

    commit tree 58d0649cb635b8c3c7e6a86ce9e2da52ccc1ec9b

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ main.remove();
 
 const newHeader = document.createElement('h1');
 newHeader.setAttribute("hi", "victory");
+newHeader.id = 'victory'
 newHeader.innerHTML = "YOUR-NAME is the champion";
 
 document.body.append(newHeader);
+
